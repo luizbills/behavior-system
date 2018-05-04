6e9d3f73cbd10755057a665f3cd1b953fae02300
Luiz Bills <luizpbills@gmail.com>
2018-05-04 00:15:15 -0300

    removeByIndex should returns undefined if the array.length == 0

--- test/remove-by-index.js
+++ test/remove-by-index.js
@@ -18,7 +18,7 @@ test('removeByIndex', function (assert) {
   removeByIndex(source, index)
   actual = 5
   expected = source[index]
-  assert.equal(actual, expected, 'should moves the last element to the index (if source.length > 1)')
+  assert.equal(actual, expected, 'should moves the last element to the index (if array.length > 1)')
 
   source = []
   index = 0
@@ -30,19 +30,19 @@ test('removeByIndex', function (assert) {
   index = source.length
   actual = undefined
   expected = removeByIndex(source, index)
-  assert.equal(actual, expected, 'should returns undefined if the index == source.length')
-
-  source = [1, 2, 3]
-  index = source.length + 1
-  actual = undefined
-  expected = removeByIndex(source, index)
-  assert.equal(actual, expected, 'should returns undefined if the index > source.length')
+  assert.equal(actual, expected, 'should returns undefined if the index >= array.length')
 
   source = [1, 2, 3]
   index = -1
   actual = undefined
   expected = removeByIndex(source, index)
   assert.equal(actual, expected, 'should returns undefined if the index < 0')
+
+  source = []
+  index = 1
+  actual = undefined
+  expected = removeByIndex(source, index)
+  assert.equal(actual, expected, 'should returns undefined if the array.length == 0')
 
   assert.end()
 })
